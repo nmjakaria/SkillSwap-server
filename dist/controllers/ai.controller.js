61ce38315d5bc9449d7113d4209371d3b66103f9
Naimullah Md Jakaria <jakaria64.nm@hotmail.com>
2026-07-20 18:25:41 +0600

    Update gitignore

--- dist/controllers/ai.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateDescription = generateDescription;
-const gemini_service_1 = require("../services/gemini.service");
-async function generateDescription(req, res) {
-    const result = await (0, gemini_service_1.generateSkillDescription)(req.body);
-    res.json(result);
-}
--- dist/routes/recommendations.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../middleware/auth");
-const recommendations_controller_1 = require("../controllers/recommendations.controller");
-const router = (0, express_1.Router)();
-router.get("/", auth_1.requireAuth, recommendations_controller_1.getRecommendations);
-exports.default = router;
